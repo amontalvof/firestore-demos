6d3a581c2eb917f2e6b8872eb773ef377301f566
amontalvof <amontalvof92@gmail.com>
2021-02-24 09:01:28 -0600

    feat: added dotenv-wepack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,42 @@
-const HtmlWebPackPlugin       = require('html-webpack-plugin'); 
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyPlugin              = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.ts',
     mode: 'development',
     resolve: {
-        extensions: ['.ts','.js','.json']
+        extensions: ['.ts', '.js', '.json'],
     },
     optimization: {
-        minimizer: [ new OptimizeCssAssetsPlugin() ]
+        minimizer: [new OptimizeCssAssetsPlugin()],
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
                 exclude: /styles\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /styles\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.html$/,
                 use: [
                     {
                         loader: 'html-loader',
-                        options: { minimize: false }
-                    }
-                ]
+                        options: { minimize: false },
+                    },
+                ],
             },
             {
                 test: /\.(png|svg|jpg|gif|ico)$/,
@@ -50,26 +45,23 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             esModule: false,
-                            name: 'assets/[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
+                            name: 'assets/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
-            filename: './index.html'
+            filename: './index.html',
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
-            ignoreOrder: false
+            ignoreOrder: false,
         }),
-        new CopyPlugin([
-            { from: 'src/favicon.ico', to: './favicon.ico' },
-        ]),
-    ]
-
-}
-
+        new CopyPlugin([{ from: 'src/favicon.ico', to: './favicon.ico' }]),
+        new Dotenv(),
+    ],
+};
