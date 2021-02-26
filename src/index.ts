e3d8bf6235ede17dfa9c2d6730346ffc3091c45a
amontalvof <amontalvof92@gmail.com>
2021-02-26 11:20:25 -0600

    feat: where clause

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,26 @@ const usersRef = db.collection('users');
 // deleteDoc(usersRef, 'kXR4FPQaltbp120GpQaw');
 
 /*
- * onSnapshot is pending any change in the database through sockets
+ ! onSnapshot is pending any change in the database through sockets
  */
 // usersRef.onSnapshot(selectAll);
 
-usersRef.get().then(selectAll);
+/*
+? select * from users
+*/
+// usersRef.get().then(selectAll);
+
+/*
+? select * from users where salary <= 1800
+*/
+// usersRef.where('salary', '<=', 1800).get().then(selectAll);
+
+/*
+? select * from users where salary >= 1800 and salary <= 2300
+! for this compound query I need to create an index in Firestore, I do it by clicking on the link that appears in the console error
+*/
+// usersRef
+//     .where('salary', '>=', 1800)
+//     .where('active', '==', true)
+//     .get()
+//     .then(selectAll);
