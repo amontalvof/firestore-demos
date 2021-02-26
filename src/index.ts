497a08d372fd0017c512fb5da0ada6cde9e3074f
amontalvof <amontalvof92@gmail.com>
2021-02-26 12:20:09 -0600

    feat: orderBy clause

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ import db from './firebase/config';
 import { selectAll } from './helpers/select';
 
 const user = {
-    name: 'Maria',
-    active: true,
+    name: 'Susana',
+    active: false,
     birthDate: 0,
+    salary: 1950,
 };
 
 const usersRef = db.collection('users');
@@ -36,10 +37,16 @@ const usersRef = db.collection('users');
 
 /*
 ? select * from users where salary >= 1800 and salary <= 2300
-! for this compound query I need to create an index in Firestore, I do it by clicking on the link that appears in the console error
+! for this compound query I need to create an index in Firestore, I do it by clicking on the link that appears in the console error or you can create it directly in Firestore
 */
 // usersRef
 //     .where('salary', '>=', 1800)
 //     .where('active', '==', true)
 //     .get()
 //     .then(selectAll);
+
+/*
+? select * from users orderBy name desc, salary asc
+! the orderBy clause if the field where you are ordering does not exist in a doc, excludes it from the data. If we have multiple orderBy we have to create an index
+*/
+// usersRef.orderBy('name', 'desc').orderBy('salary').get().then(selectAll);
